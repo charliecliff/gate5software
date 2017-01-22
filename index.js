188e891e43885a5e14c7bc59feca41b987605efd
ccliff <charlie.cliff@gmail.com>
2017-01-22 07:36:12 -0600

    Cleaning up the EJS files into sectional components

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/wings');
+  response.render('pages/main');
 });
 
 app.listen(app.get('port'), function() {
